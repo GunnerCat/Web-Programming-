76f6f08e2623258dfcac0720eb9b66fae98be922
Pieter Riyanto <ptrryt@gmail.com>
2022-12-09 04:29:43 +0700

    TEMP DUMMY

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Item;
 use Illuminate\Database\Seeder;
 
 class ItemSeeder extends Seeder
@@ -13,6 +14,31 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $items = [
+            [
+                'category_id'=>1,
+                'name'=>"Los Pollos Fermanos gunns",
+                'detail'=>"DIUTHIDUFGJNKSDLCXMFVSXKLZMC",
+                'price'=>1142124,
+                'photo'=>"odifngjmkiodmfg",
+            ],
+            [
+                'category_id'=>2,
+                'name'=>"Make ups for the REAL MANNN by pharah los carlos",
+                'detail'=>"DIUTHIDUFGJNKSDLCXMFVSXKLZMC",
+                'price'=>61212412,
+                'photo'=>"odifngjmkiodmfg",
+            ],
+            [
+                'category_id'=>3,
+                'name'=>"Actuall gun",
+                'detail'=>"sfasa",
+                'price'=>61212412,
+                'photo'=>"odifngjmkiodmfg",
+            ],
+        ];
+        foreach ($items as $item) {
+            Item::create($item);
+        }
     }
 }
