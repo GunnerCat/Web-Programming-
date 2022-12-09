4cfe3ba578509bef06d9e78bf5e8fbcc760acf48
Pieter Riyanto <ptrryt@gmail.com>
2022-12-09 22:50:55 +0700

    seedings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             CountryTableSeeder::class,
-            ItemSeeder::class
+            ItemSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'=>'Pieter Riyanto',
+                'email'=>"ptrryt@gmail.com",
+                'password'=> '$2y$10$I/bdz88GJeCbqpxSJU/xgu3zm1tTquqAfccLGH8jxHN5DIbKSUEUq',
+                'gender'=>'Male',
+                'admin'=>true,
+                'dob'=>'2022-10-10',
+                'country'=>'Indonesia',
+            ],
+
+        ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
