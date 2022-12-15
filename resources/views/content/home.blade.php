6c22e1000c16cacb9cc7960e4a106d611c2710de
Warren2032 <115253700+Warren2032@users.noreply.github.com>
2022-12-15 17:26:54 +0700

    Styling Tweaks for better image 2

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -30,10 +30,8 @@
                     <div class="container">
                         <div class="d-flex justify-content-auto row row-cols-5 ">
                             @foreach($categoryItems as $item)
-                            <a class="col text-decoration-none d-flex flex-column"  href="{{route('productDetail',$item['id'])}}" >
-                                <div>
-                                    <img  src="{{Storage::url($item['photo'])}}" alt="Responsive image" class="cont-img">
-                                </div>
+                            <a class="col text-decoration-none d-flex flex-column" href="{{route('productDetail',$item['id'])}}" >
+                                <img class="cont-img" src="{{Storage::url($item['photo'])}}" alt="Responsive image">
                                 <div class="position-sticky top-100">
                                     <div class="row">
                                         <span class="d-inline-block text-truncate" style="max-width:300px; ">{{$item['name']}} </span>
